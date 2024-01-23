811ca3fa04b8880dbd2a719adfea907853593335
min22002 <min22002@byui.edu>
2024-01-23 01:42:13 +0100

    update repo

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
+
+
+
+
 
 const port = process.env.PORT || 8085;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Listening and node Runing on port ${port}!`)});
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+    console.log(`Database is listening and node Runing on port ${port}!`)});
+        }});
